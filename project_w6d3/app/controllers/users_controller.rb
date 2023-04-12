fc3c180bb5753eb672908b7324f587a2fbddbccd
vinicozzi <vinicozzimincent@gmail.com>
2023-04-12 14:34:24 -0400

    ethan

--- project_w6d3/app/controllers/users_controller.rb
+++ project_w6d3/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController 
 
-    # def index
-    #     @users = User.all 
-    #     render json: @users 
-    # end
+    def index
+        # @users = User.all 
+        # render json: @users 
+    end
 
 
 end 
