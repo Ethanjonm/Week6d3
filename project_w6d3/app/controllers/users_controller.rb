f574faf1751afc8c1d231119618419276c61a40c
vinicozzi <vinicozzimincent@gmail.com>
2023-04-12 16:29:57 -0400

    final commit

--- project_w6d3/app/controllers/users_controller.rb
+++ project_w6d3/app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ class UsersController < ApplicationController
         end 
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user 
+    end
+
+    def update 
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            render json: @user 
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+
+    end
+
+    def user_params 
+        params.require(:user).permit(:name, :email)
+    end 
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        # render json: @user 
     end
 end 
