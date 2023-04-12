b9dc7ae55fa86c124cd023b3b498a91fff3db87d
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-12 14:53:44 -0400

    phase 3

--- project_w6d3/app/controllers/users_controller.rb
+++ project_w6d3/app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController 
 
     def index
+        render plain: "I'm in the index action!"
         # @users = User.all 
         # render json: @users 
     end
 
+    def create
+        render json: params
+    end
 
+    def show
+        render json: params
+    end
 end 
